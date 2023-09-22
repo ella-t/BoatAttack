7c7ba64fa2bef0cdcee48d5108626b1ddf65006d
ella-t <58817053+ella-t@users.noreply.github.com>
2023-09-22 16:39:45 +0100

    Detailed mode

--- TestResultsParse/parseresults.py
+++ TestResultsParse/parseresults.py
@@ -4,6 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="Unity test result XML file to parse and present.", type=argparse.FileType('r'))
+parser.add_argument("-d", "--detailed", help="output detailed test results", action="store_true")
 args = parser.parse_args()
 
 root = None
@@ -16,25 +17,37 @@ def get_test_xml():
 
 def present_test_header():
     if type(root) is ET.Element:
-        headerstring = "BoatAttack Startup Test Results\n"
+        headerstring = "\nBoatAttack Startup Test Results\n"
         headerstring += "{} total tests\n".format(root.attrib["total"])
         headerstring += "{} passed, {} failed ".format(root.attrib["passed"], root.attrib["failed"])
         if int(root.attrib["failed"]) > 0:
             headerstring += "❌"
         else:
             headerstring += "✔"
-        print(headerstring)
+        headerstring += "\n"
+        return headerstring
 
 
-def present_test_fixture():
-    pass
-
-
-def present_tests():
-    pass
+def present_test_fixture(fixture):
+    fixturestring = "{} Fixture Results\n".format(fixture.attrib["name"])
+    for test in fixture.findall("test-case"):
+        fixturestring += test.attrib["name"]
+        if test.attrib["result"] == "Passed":
+            fixturestring += " - Passed ✔ - "
+        elif test.attrib["result"] == "Failed":
+            fixturestring += " - Failed ❌ - "
+        fixturestring += "{} elapsed".format(test.attrib["duration"])
+        fixturestring += "\n"
+        if test.attrib["result"] == "Failed":
+            fixturestring += test.find("failure").find("message").text
+            fixturestring += "\n"
+    return fixturestring
 
 
 if __name__ == "__main__":
     get_test_xml()
-    present_test_header()
-    present_tests()
+    print(present_test_header())
+    if args.detailed:
+        for i in root.iter("test-suite"):
+            if i.attrib["type"] == "TestFixture":
+                print(present_test_fixture(i))
--- test-boatattack.bat
+++ test-boatattack.bat
@@ -1,2 +1,3 @@
 D:\program_files\Unity\2020.3.23f1\Editor\Unity.exe -batchmode -projectPath "." -runTests -testResults playmodetests.xml -testPlatform playmode -testCategory "BatchmodeTest"
 python TestResultsParse\parseresults.py playmodetests.xml
+pause
